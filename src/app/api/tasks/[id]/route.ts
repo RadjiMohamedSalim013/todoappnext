1255090be69622376838cd6375724ec3411b2fae
Radji Mohamed <radjimohamed013@gmail.com>
2025-06-06 11:49:57 +0100

    correction du bug lié au buil lors de l'hebergement

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -2,30 +2,39 @@ import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/mongo";
 import { lireTacheParId, modifierTache, supprimerTache } from "@/gateway/taskGateway";
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  context: { params: { id: string } }
+) {
   await connectDB();
 
-  const tache = await lireTacheParId(params.id);
+  const tache = await lireTacheParId(context.params.id);
   if (!tache) return NextResponse.json({ message: "Tâche non trouvée" }, { status: 404 });
 
   return NextResponse.json(tache);
 }
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+export async function PUT(
+  request: Request,
+  context: { params: { id: string } }
+) {
   await connectDB();
 
   const data = await request.json();
 
-  const tacheModifiee = await modifierTache(params.id, data);
+  const tacheModifiee = await modifierTache(context.params.id, data);
   if (!tacheModifiee) return NextResponse.json({ message: "Tâche non trouvée" }, { status: 404 });
 
   return NextResponse.json(tacheModifiee);
 }
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  request: Request,
+  context: { params: { id: string } }
+) {
   await connectDB();
 
-  const tacheSupprimee = await supprimerTache(params.id);
+  const tacheSupprimee = await supprimerTache(context.params.id);
   if (!tacheSupprimee) return NextResponse.json({ message: "Tâche non trouvée" }, { status: 404 });
 
   return NextResponse.json({ message: "Tâche supprimée" });
